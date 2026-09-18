7f16dd0fd82e7c0dbfe786ffab430f9f77f81313
sri91524 <sreeravish@gmail.com>
2026-09-17 22:08:29 -0400

    Remove unused Weather Forecast files

--- obj/Release/net8.0/TaskManager.AssemblyInfo.cs
+++ obj/Release/net8.0/TaskManager.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f37826d459eca6f26019f3298a1fc522375544d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
